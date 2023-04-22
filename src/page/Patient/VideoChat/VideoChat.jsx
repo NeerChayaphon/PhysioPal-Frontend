f469662c578ffcdc220c66572624a38f800d93b2
Pirada <mploy16671@gmail.com>
2023-04-22 17:22:25 +0700

    feat: videocall page

--- src/page/Patient/VideoChat/VideoChat.jsx
+++ src/page/Patient/VideoChat/VideoChat.jsx
@@ -1,14 +1,20 @@
 // Video consultation page
-import React from 'react';
-import { useEffect, useState, useRef } from 'react';
-import Peer from 'peerjs';
-import io from 'socket.io-client';
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import Peer from "peerjs";
+import io from "socket.io-client";
 // import endCallIcon from '../../img/endcall.png';
 // import medicalIcon from '../../img/medical-report-white.png';
 // import useTokenCheck from '../../helper/tokenCheck';
-import { Link, useParams } from 'react-router-dom';
-import { Box, Flex, Button, Image } from '@chakra-ui/react';
-import { useLocation } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
+import { Box, Flex, Button, Image, IconButton } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import {
+  BsFillTelephoneXFill,
+  BsFillCameraVideoFill,
+  BsFillCameraVideoOffFill,
+} from "react-icons/bs";
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 
 const VideoChat = () => {
   const { id: userId } = useParams();
@@ -29,11 +35,11 @@ const VideoChat = () => {
   const [isVideoOff, setVideoOff] = useState(false);
 
   const location = useLocation();
-  console.log('location', location);
+  console.log("location", location);
 
   useEffect(() => {
     const newSocket = io(
-      'https://medical-consultation-api-production.up.railway.app'
+      "https://medical-consultation-api-production.up.railway.app"
     ); // connect socket
     setSocket(newSocket);
 
@@ -50,12 +56,12 @@ const VideoChat = () => {
           userVideo.current.srcObject = stream;
         }
         // call
-        myPeer.on('call', (call) => {
+        myPeer.on("call", (call) => {
           call.answer(stream);
           setCallAccepted(true);
 
           // sent user's video to other
-          call.on('stream', (userVideoStream) => {
+          call.on("stream", (userVideoStream) => {
             if (otherVideo.current) {
               otherVideo.current.srcObject = userVideoStream;
             }
@@ -63,32 +69,32 @@ const VideoChat = () => {
         });
 
         // user connection
-        newSocket.on('user-connected', (userId) => {
+        newSocket.on("user-connected", (userId) => {
           connectToNewUser(userId, stream);
         });
       });
 
     // other user disconnect
-    newSocket.on('user-disconnected', (userId) => {
+    newSocket.on("user-disconnected", (userId) => {
       if (peers[userId]) peers[userId].close();
       setCallAccepted(false);
     });
 
     // join doctor consultation room
-    myPeer.on('open', (id) => {
-      newSocket.emit('join-room', userId, id);
+    myPeer.on("open", (id) => {
+      newSocket.emit("join-room", userId, id);
     });
 
     function connectToNewUser(userId, stream) {
       const call = myPeer.call(userId, stream);
       setCallAccepted(true);
       // sent user's video to other
-      call.on('stream', (userVideoStream) => {
+      call.on("stream", (userVideoStream) => {
         if (otherVideo.current) {
           otherVideo.current.srcObject = userVideoStream;
         }
       });
-      call.on('close', () => {
+      call.on("close", () => {
         setCallAccepted(false);
       });
       peers[userId] = call;
@@ -99,7 +105,7 @@ const VideoChat = () => {
   if (stream) {
     UserVideo = (
       <video
-        className='w-auto rounded-3xl'
+        className="w-auto rounded-3xl"
         playsInline
         muted
         ref={userVideo}
@@ -111,7 +117,7 @@ const VideoChat = () => {
   if (callAccepted) {
     PartnerVideo = (
       <video
-        className='w-full rounded-3xl'
+        className="w-full rounded-3xl"
         playsInline
         ref={otherVideo}
         autoPlay
@@ -143,71 +149,83 @@ const VideoChat = () => {
   return (
     <Box>
       <Flex
-        h='100vh'
-        overflow='auto'
-        bg='gray.100'
-        p='4'
-        alignItems='center'
-        mx='6'
-        my='2'
+        h="100vh"
+        overflow="auto"
+        bg="gray.100"
+        p="4"
+        alignItems="center"
+        mx="6"
+        my="2"
       >
         <Box
-          w='50%'
-          h='lg'
-          shadow='lg'
-          rounded='lg'
-          borderRight={{ base: 'none', lg: '1px solid gray.200' }}
-          bg='#B5E3FE'
-          p='4'
+          w="50%"
+          h="lg"
+          shadow="lg"
+          rounded="lg"
+          borderRight={{ base: "none", lg: "1px solid gray.200" }}
+          bg="#B5E3FE"
+          p="4"
         >
           {UserVideo}
         </Box>
         <Box
-          w='50%'
-          h='lg'
-          shadow='lg'
-          rounded='lg'
-          borderRight={{ base: 'none', lg: '1px solid gray.200' }}
-          bg='#FFCCD0'
-          p='4'
-          ml='4'
+          w="50%"
+          h="lg"
+          shadow="lg"
+          rounded="lg"
+          borderRight={{ base: "none", lg: "1px solid gray.200" }}
+          bg="#FFCCD0"
+          p="4"
+          ml="4"
         >
           {PartnerVideo || <Box />}
         </Box>
       </Flex>
-      <Flex mx='2' p='4' gap='2' mt='8'>
-        <Box w='75%'>
-          <Flex justify='start' gap='2'>
-            <Button
+      <Flex mx="2" p="4" gap="2" mt="8">
+        <Box w="75%">
+          <Flex justify="start" gap="2">
+            {/* <Button
               onClick={mute}
-              h='12'
-              w='12'
-              rounded='lg'
-              bg='purple.500'
-              _hover={{ bg: 'purple.700' }}
+              h="12"
+              w="12"
+              rounded="lg"
+              bg="purple.500"
+              _hover={{ bg: "purple.700" }}
             >
               {isMute ? (
-                <i className='text-white fas fa-microphone-slash'></i>
+                <i className="text-white fas fa-microphone-slash"></i>
               ) : (
-                <i className='text-white  fa fa-microphone'></i>
+                <i className="text-white  fa fa-microphone"></i>
               )}
               Mute/Unmute
             </Button>
             <Button
               onClick={videoControl}
-              h='12'
-              w='12'
-              rounded='lg'
-              bg='purple.500'
-              _hover={{ bg: 'purple.700' }}
+              h="12"
+              w="12"
+              rounded="lg"
+              bg="purple.500"
+              _hover={{ bg: "purple.700" }}
             >
               {isVideoOff ? (
-                <i className='text-white fas fa-video-slash'></i>
+                <i className="text-white fas fa-video-slash"></i>
               ) : (
-                <i className='text-white fas fa-video'></i>
+                <i className="text-white fas fa-video"></i>
               )}
               Turn on/off video
-            </Button>
+            </Button> */}
+            <IconButton
+              colorScheme="teal"
+              aria-label="Call Segun"
+              size="lg"
+              icon={<FaMicrophone />}
+            />
+            <IconButton
+              colorScheme="teal"
+              aria-label="Call Segun"
+              size="lg"
+              icon={<BsFillCameraVideoFill />}
+            />
             {/* {type === 'doctor' && (
               <Link
                 to={{ pathname: `/manageMedicalRecord/${user.id}` }}
@@ -230,24 +248,14 @@ const VideoChat = () => {
             )} */}
           </Flex>
         </Box>
-        <Box w='25%'>
-          <Flex justify='end' gap='2'>
-            <Button
-              display='inline-flex'
-              h='12'
-              w='12'
-              bg='red.400'
-              _hover={{ bg: 'red.500' }}
-              fontWeight='bold'
-              rounded='lg'
-              // alignItems='center'
-              // justifyContent='center'
-              py='2'
-              px='2'
-            >
-              End call
-              {/* <Image src={endCallIcon} alt='' /> */}
-            </Button>
+        <Box w="25%">
+          <Flex justify="end" gap="2">
+            <IconButton
+              colorScheme="red"
+              aria-label="Call Segun"
+              size="lg"
+              icon={<BsFillTelephoneXFill />}
+            />
           </Flex>
         </Box>
       </Flex>
